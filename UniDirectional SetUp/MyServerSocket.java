e2e82efa55fc28a6037b446bc8af0854ac0533b2
Sadiq Ahmed Killedar <125464939+Er-Sadiq@users.noreply.github.com>
2024-03-29 11:21:05 +0530

    Add files via upload
    " one way simple connection"

---
+++ UniDirectional SetUp/MyServerSocket.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyServerSocket {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Server is started");
+            ServerSocket serSocket = new ServerSocket(9999);
+            System.out.println("Waiting for connection");
+            Socket sock = serSocket.accept();
+            System.out.println("Client is connected");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            String receivedData = br.readLine(); // Read the data sent by the client
+            System.out.println("Received data from client: " + receivedData);
+
+            // You can process the received data here as needed
+
+            br.close();
+            sock.close();
+            serSocket.close();
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e);
+        }
+    }
+}
